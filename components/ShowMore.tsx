36da450d163de465d3189d3b597e5ed2d4df12e6
Huong Tram Thi Thuy <huong.tram@stashnextgen.com>
2023-07-06 14:16:49 +0700

    project car use NextJS

---
+++ components/ShowMore.tsx
@@ -0,0 +1,31 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+
+import { ShowMoreProps } from "@types";
+import { CustomButton } from "@components";
+
+const ShowMore = ({ pageNumber, isNext, setLimit }: ShowMoreProps) => {
+  const router = useRouter();
+
+  const handleNavigation = () => {
+    const newLimit = (pageNumber + 1) * 10;
+
+    setLimit(newLimit)
+  };
+
+  return (
+    <div className="w-full flex-center gap-5 mt-10">
+      {!isNext && (
+        <CustomButton
+          btnType="button"
+          title="Show More"
+          containerStyles="bg-primary-blue rounded-full text-white"
+          handleClick={handleNavigation}
+        />
+      )}
+    </div>
+  );
+};
+
+export default ShowMore;
---
+++ components/index.tsx
@@ -0,0 +1,21 @@
+import Hero from "./Hero";
+import CustomButton from "./CustomButton";
+import NavBar from "./NavBar";
+import Footer from "./Footer";
+import SearchBar from "./SearchBar";
+import CustomFilter from "./CustomFilter";
+import CarCard from "./CarCard";
+import ShowMore from "./ShowMore";
+import CarDetails from "./CarDetails";
+
+export {
+  Hero,
+  CustomButton,
+  NavBar,
+  Footer,
+  SearchBar,
+  CustomFilter,
+  CarCard,
+  ShowMore,
+  CarDetails
+}
